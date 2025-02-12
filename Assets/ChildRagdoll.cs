bea95c9d91f24c29de98c59ba1970f8ba4476aec
acapela <acapela@NS00J002003DE01>
2025-02-12 17:02:05 +1100

    ragdool

---
+++ Assets/ChildRagdoll.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class ChildRagdoll : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        GetComponentInParent<RagdollMe>()?.Ragdoll();
+        enabled = false;
+    }
+
+}
